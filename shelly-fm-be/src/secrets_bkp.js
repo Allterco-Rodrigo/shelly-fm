a4a44ebf0bf0e0c628a17419393d057ba30ca5d2
Allterco-Rodrigo <117780107+Allterco-Rodrigo@users.noreply.github.com>
2023-06-27 10:27:32 -0400

    Delete secrets_bkp.js

--- shelly-fm-be/src/secrets_bkp.js
+++
@@ -1,8 +0,0 @@
-const gmail = {
-  EMAIL_SERVICE: 'gmail',
-  EMAIL_USERNAME: 'emails@gmail.com',
-  EMAIL_PASSWORD: 'bazinga', // config authorization for app
-  EMAIL_FROM: 'senderm@gmail.com',
-}
-
-export const EMAIL_CREDENTIALS = gmail
